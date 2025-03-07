a9adf19e8df5fad9d913be296f6c6f5fb1eee1d7
Tobias Weber <toby-weber@gmx.de>
2025-03-07 22:27:44 +0100

    feat: call blade constructor to initialize and process all blade related data

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <memory>
 #include "ProjectData.h"
+#include "Blade.h"
 int main(int argc, char** argv) {
-	//todo: read project data file and parse data to obj
+
 	std::string projectFile = argv[1];
 	std::shared_ptr<ProjectData> ptrProjectData = std::make_shared<ProjectData>(projectFile);
+
+	std::shared_ptr<Blade> ptrBlade = std::make_shared<Blade>(ptrProjectData);
+
+	
+
 	return 0;
 }
